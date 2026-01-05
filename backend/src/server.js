71632c7483312b0bcf65432df57d6b4cf422eb70
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-05 12:24:01 +0700

    feat: init server.js with /api/products endpoint
    - return temp json product data

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/api/products", (req, res) => {
+    // temp data
+    res.json([
+        { id: 1, name: "Mouse", price: 50000 },
+        { id: 2, name: "Keyboard", price: 150000 }
+    ]);
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
