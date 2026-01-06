efc12b85206bff8d49d464ce1537813c7f67a25a
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-07 06:59:19 +0700

    feat(frontend): add button hover interaction in Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "../scss/global.scss";
 
 export default function Home() {
     return (
         <div className="container py-4 text-center">
             <h1 className="display-1">Online Shop</h1>
-            <Link className="btn btn-primary btn-lg" to="/products">
+            <Link className="btn btn-primary btn-lg hover-btn" to="/products">
                 Browse More Products
             </Link>
         </div>
