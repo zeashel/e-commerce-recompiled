1f37371a4bcb38083df220a72a5c4ebb8f674dff
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-07 09:25:07 +0700

    chore(frontend): tweak Home page design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,9 +4,12 @@ import "../scss/global.scss";
 export default function Home() {
     return (
         <div className="container py-4 text-center">
-            <h1 className="display-1 fw-bold">Recompiled.</h1>
+            <h1 className="display-1 fw-bold text-decoration-underline link-underline-primary">
+                Recompiled.
+            </h1>
             <p className="text-muted lead">
-                Retro technology, carefully recompiled for today.
+                Retro technology, carefully{" "}
+                <span className=" text-body">recompiled</span> for today.
             </p>
             <Link className="btn btn-primary btn-lg hover-btn" to="/products">
                 Browse Inventory
