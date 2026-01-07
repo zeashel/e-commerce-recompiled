1ea1aace6de896d743eb974f6f13bd72d8a7d2fc
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-07 15:23:51 +0700

    fix(frontend): fix non-JSX DOM properties
    - fixed file: Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,9 +12,9 @@ function Navbar() {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="1.5"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         className="lucide text-primary"
                         style={{
                             marginInlineEnd: "0.25em",
