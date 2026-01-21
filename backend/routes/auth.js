1a44010601aaa35ec70c847d72022089abe29ea5
zahra a <182934048+zeashel@users.noreply.github.com>
2026-01-21 16:29:18 +0700

    chore(backend): initialize auth route

---
+++ backend/routes/auth.js
@@ -0,0 +1,7 @@
+// authentication route
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
